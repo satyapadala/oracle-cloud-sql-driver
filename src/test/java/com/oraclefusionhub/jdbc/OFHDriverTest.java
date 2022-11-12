8d4bdf09741a3029e1754e7a1644c5a317b3d6c0
Satya Padala <spadala@itconvergence.com>
2022-11-12 14:06:55 +0530

    Misc Refactor and more tests

--- src/test/java/com/oraclefusionhub/jdbc/OFHDriverTest.java
+++ src/test/java/com/oraclefusionhub/jdbc/OFHDriverTest.java
@@ -1,10 +1,65 @@
 package com.oraclefusionhub.jdbc;
 
+import static org.junit.Assert.assertTrue;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
 public class OFHDriverTest {
-	@Test
-	public void driver_works_in_normal_case() {
+
+	private static Driver driver;
+
+	@Rule
+	public WireMockRule mockXMLPServer = new WireMockRule(8089);
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		driver = new OFHDriver();
+
+		DriverManager.registerDriver(driver);
 
 	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		DriverManager.deregisterDriver(driver);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@Test
+	public void test() throws SQLException {
+
+		mockXMLPServer.stubFor(WireMock.post(WireMock.urlEqualTo("/xmlpserver/services/ExternalReportWSSService"))
+				.willReturn(WireMock.aResponse().withStatus(200).withHeader("Content-Type", "text/xml")
+						.withBodyFile("E2ETestResult.xml")));
+
+		Connection connection = DriverManager.getConnection("jdbc:ofh://http://localhost:8089", "testUser",
+				"testPassword");
+
+		Statement statement = connection.createStatement();
+
+		ResultSet resultSet = statement.executeQuery("select * from ofh_user");
+
+		assertTrue("resultset should contain data", resultSet.next());
+
+		connection.close();
+	}
+
 }
--- src/test/java/com/oraclefusionhub/jdbc/OFHResultSetTest.java
+++ src/test/java/com/oraclefusionhub/jdbc/OFHResultSetTest.java
@@ -1,8 +1,10 @@
 package com.oraclefusionhub.jdbc;
 
-import junit.framework.TestCase;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -10,8 +12,9 @@ import java.io.StringReader;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class OFHResultSetTest extends TestCase {
+public class OFHResultSetTest {
 
+    @Test
     public void testResultSet() throws IOException, SQLException {
         Reader in = new StringReader("a,b\na1,b1\na2,\"b2 and b2 something\"");
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(in);
@@ -29,6 +32,8 @@ public class OFHResultSetTest extends TestCase {
         rs.next();
         assertEquals("a2", rs.getString(1));
         assertEquals("b2 and b2 something", rs.getString(2));
+
+        rs.close();
     }
 
 }
